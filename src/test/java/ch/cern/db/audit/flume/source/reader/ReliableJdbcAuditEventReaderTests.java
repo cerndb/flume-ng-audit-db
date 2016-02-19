b562293d5b6a46e999a5bc365d6ed376d1aba111
Daniel Lanza Garcia <dlanza1@gmail.com>
2016-02-19 12:13:09 +0100

    New tests using in-memory db

--- src/test/java/ch/cern/db/audit/flume/source/reader/ReliableJdbcAuditEventReaderTests.java
+++ src/test/java/ch/cern/db/audit/flume/source/reader/ReliableJdbcAuditEventReaderTests.java
@@ -4,21 +4,89 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.flume.Context;
+import org.apache.flume.Event;
 import org.apache.flume.FlumeException;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+@SuppressWarnings("resource")
 public class ReliableJdbcAuditEventReaderTests {
+	
+	String connection_url = "jdbc:hsqldb:mem:aname";
+	Connection connection = null;
+	
+	@Before
+	public void setup(){
+		try {
+			connection = DriverManager.getConnection(connection_url, "sa", "");
+			
+			Statement statement = connection.createStatement();
+			statement.execute("DROP TABLE IF EXISTS audit_data_table;");
+			statement.execute("CREATE TABLE audit_data_table (id INTEGER, return_code BIGINT, name VARCHAR(20));");
+			statement.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void eventsFromDatabase(){
+		
+		Context context = new Context();
+		context.put(ReliableJdbcAuditEventReader.CONNECTION_DRIVER_PARAM, "org.hsqldb.jdbc.JDBCDriver");
+		context.put(ReliableJdbcAuditEventReader.CONNECTION_URL_PARAM, connection_url);
+		context.put(ReliableJdbcAuditEventReader.USERNAME_PARAM, "SA");
+		context.put(ReliableJdbcAuditEventReader.PASSWORD_PARAM, "");
+		context.put(ReliableJdbcAuditEventReader.TABLE_NAME_PARAM, " audit_data_table");
+		context.put(ReliableJdbcAuditEventReader.COLUMN_TO_COMMIT_PARAM, "ID");
+		ReliableJdbcAuditEventReader reader = new ReliableJdbcAuditEventReader(context);
+		
+		try {
+			Event event = reader.readEvent();
+			Assert.assertNull(event);
+			
+			Statement statement = connection.createStatement();
+			statement.execute("INSERT INTO audit_data_table VALUES 2, 48, 'name2';");
+			event = reader.readEvent();
+			Assert.assertNotNull(event);
+			Assert.assertEquals("{\"ID\":2,\"RETURN_CODE\":48,\"NAME\":\"name2\"}", new String(event.getBody()));
+			event = reader.readEvent();
+			Assert.assertNull(event);
+			
+			statement = connection.createStatement();
+			statement.execute("INSERT INTO audit_data_table VALUES 1, 48, 'name1';");
+			statement.execute("INSERT INTO audit_data_table VALUES 3, 48, 'name3';");
+			statement.close();
+			event = reader.readEvent();
+			Assert.assertNotNull(event);
+			Assert.assertEquals("{\"ID\":1,\"RETURN_CODE\":48,\"NAME\":\"name1\"}", new String(event.getBody()));
+			event = reader.readEvent();
+			Assert.assertNotNull(event);
+			Assert.assertEquals("{\"ID\":3,\"RETURN_CODE\":48,\"NAME\":\"name3\"}", new String(event.getBody()));
+			event = reader.readEvent();
+			Assert.assertNull(event);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void createCommitFile(){
 		Context context = new Context();
 		context.put(ReliableJdbcAuditEventReader.TABLE_NAME_PARAM, "table");
 		context.put(ReliableJdbcAuditEventReader.COLUMN_TO_COMMIT_PARAM, "column");
-		@SuppressWarnings("resource")
 		ReliableJdbcAuditEventReader reader = new ReliableJdbcAuditEventReader(context);
 		
 		String timestamp = "2016-02-09 09:34:51.244507 Europe/Zurich";
@@ -61,7 +129,6 @@ public class ReliableJdbcAuditEventReaderTests {
 		Context context = new Context();
 		context.put(ReliableJdbcAuditEventReader.TABLE_NAME_PARAM, "table");
 		context.put(ReliableJdbcAuditEventReader.COLUMN_TO_COMMIT_PARAM, "column");
-		@SuppressWarnings("resource")
 		ReliableJdbcAuditEventReader reader = new ReliableJdbcAuditEventReader(context);
 		
 		Assert.assertEquals(timestamp, reader.committed_value);
@@ -74,7 +141,6 @@ public class ReliableJdbcAuditEventReaderTests {
 		Context context = new Context();
 		context.put(ReliableJdbcAuditEventReader.TABLE_NAME_PARAM, "table");
 		context.put(ReliableJdbcAuditEventReader.COLUMN_TO_COMMIT_PARAM, "column");
-		@SuppressWarnings("resource")
 		ReliableJdbcAuditEventReader reader = new ReliableJdbcAuditEventReader(context);
 		Assert.assertNull(reader.committed_value);
 		
@@ -106,7 +172,6 @@ public class ReliableJdbcAuditEventReaderTests {
 	}
 	
 	@Test
-	@SuppressWarnings("resource")
 	public void queryCreation(){
 		
 		Context context = new Context();
@@ -212,12 +277,15 @@ public class ReliableJdbcAuditEventReaderTests {
 			reader = new ReliableJdbcAuditEventReader(context);
 			Assert.fail();
 		}catch(FlumeException e){
-			System.out.println(e.getMessage());
 		}
 	}
 
 	@After
 	public void cleanUp(){
 		new File(ReliableJdbcAuditEventReader.COMMITTING_FILE_PATH_DEFAULT).delete();
+		
+		try {
+			connection.close();
+		} catch (SQLException e) {}
 	}
 }
