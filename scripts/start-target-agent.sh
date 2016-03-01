e835a898a1e88517f64ebff72b62b05752da3eb3
Daniel Lanza Garcia <dlanza1@gmail.com>
2016-03-01 18:02:26 +0100

    Update start-target-agent.sh

--- scripts/start-target-agent.sh
+++ scripts/start-target-agent.sh
@@ -1,5 +1,5 @@
 flume-ng agent \
 --classpath ../target/flume-ng-customizations-0.0.1-SNAPSHOT.jar \
 -n audit_agent \
--c ../../lib/ElasticsearchSink2/build/libs/elasticsearch-sink2-assembly-1.0.jar \
+-c ../lib/elasticsearch-sink2-1.0.jar \
 -f ../conf/target/agent.conf 
