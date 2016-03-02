f2cdbb136902ac4af43ebb3043195ac48a77bc5e
Daniel Lanza Garcia <dlanza1@gmail.com>
2016-03-02 09:09:28 +0100

    Start scripts can be executed from everywhere

--- scripts/start-source-agent.sh
+++ scripts/start-source-agent.sh
@@ -1,5 +1,20 @@
-/usr/lib/aimon-flume-ng/bin/flume-ng agent \
---classpath $ORACLE_HOME/jdbc/lib/ojdbc8.jar:../src/main/java/ \
--n audit_agent \
--c ../conf/source/ \
--f ../conf/source/agent.conf  
+#!/bin/bash
+
+# Reference: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+HOME=../$SCRIPT_DIR
+
+FLUME_BIN_DIR=/usr/lib/aimon-flume-ng/bin/
+
+$FLUME_BIN_DIR/flume-ng agent \
+	--classpath $ORACLE_HOME/jdbc/lib/ojdbc8.jar:$HOME/src/main/java/ \
+	-n audit_agent \
+	-f $HOME/conf/source/agent.conf \
+	-c $HOME/conf/source/
--- scripts/start-target-agent.sh
+++ scripts/start-target-agent.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+# Reference: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+HOME=../$SCRIPT_DIR
+
 flume-ng agent \
---classpath ../target/flume-ng-customizations-0.0.1-SNAPSHOT.jar \
--n audit_agent \
--c ../lib/elasticsearch-sink2-1.0.jar \
--f ../conf/target/agent.conf 
+	--classpath $HOME/target/flume-ng-customizations-0.0.1-SNAPSHOT.jar \
+	-n audit_agent \
+	-f $HOME/conf/target/agent.conf \
+	-c $HOME/lib/elasticsearch-sink2-1.0.jar 
