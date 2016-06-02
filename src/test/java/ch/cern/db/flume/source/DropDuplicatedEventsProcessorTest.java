e5ee8bbe40dabd7b040597ab5bf48518530c1a99
Daniel Zolnai <daniel.zolnai@cern.ch>
2016-06-02 12:14:29 +0200

    [MONIT-228] Add MD5 hashing option

--- src/test/java/ch/cern/db/flume/source/DropDuplicatedEventsProcessorTest.java
+++ src/test/java/ch/cern/db/flume/source/DropDuplicatedEventsProcessorTest.java
@@ -9,17 +9,27 @@
 package ch.cern.db.flume.source;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.event.EventBuilder;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class DropDuplicatedEventsProcessorTest {
+
+	public static @DataPoints int[] sizes = {10, 1001};
 	
 	@Test
 	public void reconfigure(){
@@ -180,7 +190,7 @@ public class DropDuplicatedEventsProcessorTest {
 		Assert.assertEquals(2, b2_events_intercepted.size());
 		Assert.assertSame(e3, b2_events_intercepted.get(0));
 		Assert.assertSame(e4, b2_events_intercepted.get(1));
-		
+
 		interceptor.rollback();
 		// 2222 1111
 		
@@ -198,15 +208,15 @@ public class DropDuplicatedEventsProcessorTest {
 	}
 	
 	@Test
-	public void persistingHash(){
+	public void persistingHash() {
 		new File("src/test/resources/last.hashes").delete();
-		
+
 		Context context = new Context();
 		context.put(DropDuplicatedEventsProcessor.SIZE_PARAM, "2");
 		context.put(DropDuplicatedEventsProcessor.PATH_PARAM, "src/test/resources/last.hashes");
 		DropDuplicatedEventsProcessor interceptor = new DropDuplicatedEventsProcessor();
 		interceptor.configure(context);
-		
+
 		//Batch 1
 		LinkedList<Event> b1_events = new LinkedList<Event>();
 		Event e1 = EventBuilder.withBody("1111".getBytes());
@@ -217,13 +227,13 @@ public class DropDuplicatedEventsProcessorTest {
 		Assert.assertEquals(2, b1_events_intercepted.size());
 		Assert.assertSame(e1, b1_events_intercepted.get(0));
 		Assert.assertSame(e2, b1_events_intercepted.get(1));
-		
+
 		interceptor.commit();
 		interceptor = new DropDuplicatedEventsProcessor();
 		interceptor.configure(context);
 		//2222 		1111
 		//[2462721, 2431937]
-		
+
 		//Batch 2
 		LinkedList<Event> b2_events = new LinkedList<Event>();
 		Event e3 = EventBuilder.withBody("3333".getBytes());
@@ -233,13 +243,13 @@ public class DropDuplicatedEventsProcessorTest {
 		List<Event> b2_events_intercepted = interceptor.process(b2_events);
 		Assert.assertEquals(1, b2_events_intercepted.size());
 		Assert.assertSame(e3, b2_events_intercepted.get(0));
-		
+
 		interceptor.commit();
 		interceptor = new DropDuplicatedEventsProcessor();
 		interceptor.configure(context);
 		//3333 2222
 		//[2493505, 2462721] 
-		
+
 		//Batch 3
 		LinkedList<Event> b3_events = new LinkedList<Event>();
 		Event e5 = EventBuilder.withBody("1111".getBytes());
@@ -251,6 +261,97 @@ public class DropDuplicatedEventsProcessorTest {
 		List<Event> b3_events_intercepted = interceptor.process(b3_events);
 		Assert.assertEquals(1, b3_events_intercepted.size());
 		Assert.assertSame(e5, b3_events_intercepted.get(0));
+	}
+
+	/**
+	 * This test runs parametrized, so first with a size of 2, then with a size of 1001.
+	 * This results in using two different algorithms, first the Java hashcode, then MD5.
+	 * Results should be the same.
+	 * @param size The preferred size of the processor.
+     */
+	@Theory
+	public void testHeaderHashing(int size) {
+		Context context = new Context();
+		context.put(DropDuplicatedEventsProcessor.SIZE_PARAM, String.valueOf(size));
+		context.put(DropDuplicatedEventsProcessor.CHECK_HEADER_PARAM, Boolean.TRUE.toString());
+		context.put(DropDuplicatedEventsProcessor.CHECK_BODY_PARAM, Boolean.FALSE.toString());
+		DropDuplicatedEventsProcessor interceptor = new DropDuplicatedEventsProcessor();
+		interceptor.configure(context);
+
+		// header1 and header2 match, header3 doesn't
+		Map<String, String> header1 = new HashMap<String, String>();
+		header1.put("foo", "bar");
+		header1.put("test", "this");
+
+		Map<String, String> header2 = new HashMap<String, String>();
+		header2.put("foo", "bar");
+		header2.put("test", "this");
+
+		Map<String, String> header3 = new HashMap<String, String>();
+		header3.put("foo", "not bar");
+		header3.put("test", "not this");
+
+		//Batch 1
+		LinkedList<Event> b1_events = new LinkedList<Event>();
+		Event e1 = EventBuilder.withBody("1111".getBytes(), header1);
+		b1_events.add(e1);
+		Event e2 = EventBuilder.withBody("2222".getBytes(), header2);
+		b1_events.add(e2);
+		List<Event> b1_events_intercepted = interceptor.process(b1_events);
+		// Body doesn't equal but header does, so one of them will be intercepted
+		Assert.assertEquals(1, b1_events_intercepted.size());
+		interceptor.commit();
+
+		//Batch 2
+		LinkedList<Event> b2_events = new LinkedList<Event>();
+		Event e3 = EventBuilder.withBody("3333".getBytes(), header1);
+		b2_events.add(e3);
+		Event e4 = EventBuilder.withBody("4444".getBytes(), header3);
+		b2_events.add(e4);
+		List<Event> b2_events_intercepted = interceptor.process(b2_events);
+		// e3's header matches the one from the previous batch, e4's doesn't match any
+		Assert.assertEquals(1, b2_events_intercepted.size());
+	}
+
+	/**
+	 * This test runs parametrized, so first with a size of 2, then with a size of 1001.
+	 * This results in using two different algorithms, first the Java hashcode, then MD5.
+	 * Results should be the same.
+	 * @param size The preferred size of the processor.
+	 */
+	@Theory
+	public void testBodyHashing(int size) {
+		Context context = new Context();
+		context.put(DropDuplicatedEventsProcessor.SIZE_PARAM, String.valueOf(size));
+		context.put(DropDuplicatedEventsProcessor.CHECK_HEADER_PARAM, Boolean.FALSE.toString());
+		context.put(DropDuplicatedEventsProcessor.CHECK_BODY_PARAM, Boolean.TRUE.toString());
+		DropDuplicatedEventsProcessor interceptor = new DropDuplicatedEventsProcessor();
+		interceptor.configure(context);
+
+		String body1 = "{\"foo\":\"bar\"}";
+		String body2 = "{\"foo\":\"bar\"}";
+		String body3 = "{\"bar\":\"foo\"}";
+
+		//Batch 1
+		LinkedList<Event> b1_events = new LinkedList<Event>();
+		Event e1 = EventBuilder.withBody(body1.getBytes());
+		b1_events.add(e1);
+		Event e2 = EventBuilder.withBody(body2.getBytes());
+		b1_events.add(e2);
+		List<Event> b1_events_intercepted = interceptor.process(b1_events);
+		// The two bodies equal
+		Assert.assertEquals(1, b1_events_intercepted.size());
+		interceptor.commit();
+
+		//Batch 2
+		LinkedList<Event> b2_events = new LinkedList<Event>();
+		Event e3 = EventBuilder.withBody(body2.getBytes());
+		b2_events.add(e3);
+		Event e4 = EventBuilder.withBody(body3.getBytes());
+		b2_events.add(e4);
+		List<Event> b2_events_intercepted = interceptor.process(b2_events);
+		// e3 equals e2 from previous batch. e4 does not equal any previous event.
+		Assert.assertEquals(1, b2_events_intercepted.size());
 	}
 	
 	@After
